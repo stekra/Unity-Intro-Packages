bf4a46eac7569b054aeac3ec2fe4701ec6f835a7
stekra <stekra@me.com>
2021-03-07 23:22:11 +0100

    First commit

---
+++ ChangeSceneOnTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneOnTrigger : MonoBehaviour
+{
+    public string SceneToChangeTo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneToChangeTo);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Willkommen in der GitHub Repo!
+Hier findet ihr alle Ressourcen für die Unity Einführung.
+
+Zum herunterladen klickt auf den grünen Button `↓ Code` oben und wählt `Download ZIP`.
